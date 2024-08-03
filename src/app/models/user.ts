bdbed8d085f1f725770b9d0ff422e579f846be61
Oksana Karpyn <oksana101992@gmail.com>
2024-08-03 12:49:18 +0200

    difine user model

---
+++ src/app/models/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+    name:string;
+    surname:string;
+    fiscalCode:string;
+    address?:string;
+    cap?:number;
+    city?:string;
+    country?:string;
+    phone?:string; //for international num
+    mobile:string; //for international num
+    email:string;
+}
