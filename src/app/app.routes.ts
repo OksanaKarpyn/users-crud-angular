3cfe721f094f0ece0fca5a5471599ff80b89c866
Oksana Karpyn <oksana101992@gmail.com>
2024-08-03 16:14:16 +0200

    route list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UsersComponent } from './pages/users/users.component';
+import { AddEditUserComponent } from './pages/add-edit-user/add-edit-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home Page',
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+    title: 'List of Users',
+  },
+  {
+    path: 'users/add-edit',
+    component: AddEditUserComponent,
+    title: 'Add Edit User',
+  },
+];
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { UnderNavbarComponent } from '../under-navbar/under-navbar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [UnderNavbarComponent],
+  imports: [UnderNavbarComponent, RouterLink],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
-export class NavBarComponent {
-
-}
+export class NavBarComponent {}
