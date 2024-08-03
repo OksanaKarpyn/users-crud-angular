4f487afe98686d951525e7a7109eb06bc8d76195
Oksana Karpyn <oksana101992@gmail.com>
2024-08-03 20:01:06 +0200

    add table and actions call get

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { User } from '../../models/user';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.scss'
+  styleUrl: './users.component.scss',
 })
 export class UsersComponent {
+  users: Array<User> = [];
 
+  constructor(private usersService: UsersService) {
+    usersService.getUsers().subscribe({
+      next: (data) => {
+        this.users = data;
+      },
+      error: (err) => {
+        console.error(err.massege);
+      },
+    });
+  }
 }
