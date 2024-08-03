6ce5c59d4e35091f2dcd04a86a8818195e531550
Oksana Karpyn <oksana101992@gmail.com>
2024-08-03 16:44:26 +0200

    add active route

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { UnderNavbarComponent } from '../under-navbar/under-navbar.component';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [UnderNavbarComponent, RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
