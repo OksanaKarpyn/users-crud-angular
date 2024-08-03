c95137b6c9425773ce65b2bf1c961739fc77ce50
Oksana Karpyn <oksana101992@gmail.com>
2024-08-03 18:23:18 +0200

    statr reactive form

--- src/app/pages/add-edit-user/add-edit-user.component.ts
+++ src/app/pages/add-edit-user/add-edit-user.component.ts
@@ -1,12 +1,32 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-add-edit-user',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './add-edit-user.component.html',
-  styleUrl: './add-edit-user.component.scss'
+  styleUrl: './add-edit-user.component.scss',
 })
 export class AddEditUserComponent {
+  form!: UntypedFormGroup;
 
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(15),
+        ]),
+      ],
+      surname: [],
+    });
+  }
 }
