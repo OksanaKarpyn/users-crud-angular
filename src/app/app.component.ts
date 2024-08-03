82d7b68d52355c5c1a94c85244f2cd57000573b1
Oksana Karpyn <oksana101992@gmail.com>
2024-08-03 16:01:00 +0200

    create pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    NavBarComponent,
-
-  ],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
-  styleUrls:['./app.component.scss' ] 
+  // styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  
-}
+export class AppComponent {}
