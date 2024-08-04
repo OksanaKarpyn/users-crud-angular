28ebcd9af1928763c4843680e01d735837311d15
Oksana.Karpyn <oksana101992@gmail.com>
2024-08-04 20:11:20 +0200

    add users in page home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,28 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { User } from '../../models/user';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe,RouterLink],
   templateUrl: './home.component.html',
-  // styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss'
 })
-export class HomeComponent {}
+export class HomeComponent {
+  //<Users> si riferisce da interfaccia ogetto definito nel modelss
+  users:Array<User> = []
+  constructor( private userservices:UsersService){
+    userservices.getUsers().subscribe({
+      next: (dataUsers)=>{
+        this.users = dataUsers;
+      },
+      error: (err) => {
+        console.error(err.massege);
+      },
+    })
+  }
+
+}
