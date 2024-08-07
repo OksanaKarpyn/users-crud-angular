3844e86b7391f15c2fac4ec28da37a02fd9f3c2a
Oksana.Karpyn <oksana101992@gmail.com>
2024-08-07 18:00:48 +0200

    add login component

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule,JsonPipe } from '@angular/common';
+import { UsersService } from '../../services/users.service';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
